7a0396b93fed050bafc801504521b354cb08d96b
henrik.prangel <henrik.prangel@netgroup.com>
2025-05-21 16:56:12 +0300

    refactor: Add homepage menuitem

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -50,6 +50,13 @@ export function NavBar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
           <NavigationMenu>
             <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                  <Link href="/">
+                    LifeAid
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
 
               <NavigationMenuItem>
                 <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
